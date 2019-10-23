2de163df97996e82f9adc311e4a31014e4ff62d2
Richu A Kuttikattu <richuak@gitlab.com>
2019-10-23 09:21:27 +0530

    Modifier and mapping added to Choder_helper

--- contracts/Chorder_Helper.sol
+++ contracts/Chorder_Helper.sol
@@ -17,7 +17,31 @@
 pragma solidity ^0.5.0;
 
 contract Chorder_Helper{
+
+    constructor () public {
+        owner = msg.sender;
+    }
+    address owner;
+    address Chorder_contract;
+
     //mapping from tokenId to resalePrice
     mapping (uint256 => uint256) internal reSalePrices;
+
+    //Modifier for onlyOwner functions
+    modifier onlyOwner {
+       require(msg.sender == owner, 'You are not the contract owner to call this function!');
+       _;
+    }
+
+    //Function to set the Chorder_contract address, only to be called by the contract deployer
+    function setChorderAddress(address _chorderAddress) public onlyOwner{
+        Chorder_contract = _chorderAddress;
+    }
+
+    //Function to set the values of reSalePrices mapping, to be called from Chorder_contract
+    function setResalePrices(uint256 _tokenid, uint256 _resalePrice) public{
+        require(msg.sender == Chorder_contract, "You are not authorized to call this function!");
+        reSalePrices[_tokenid] = _resalePrice;
+    }
 
 }
